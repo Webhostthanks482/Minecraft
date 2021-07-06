7757a3c344d3b43a9807da8367fa8cab425dfd1a
Pranav <83628215+Pranav18262@users.noreply.github.com>
2021-07-06 10:00:50 +0530

    ehh

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -14,7 +14,7 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 4726773
+    API_ID = "4726773"
     API_HASH = "34fa7c7ece67fbadb63f7823172e46a3"
     STRING_SESSION = "1AZWarzgBu3ecdnighnLB8jtb0rIIZu2UMbrmVLhEiEeGRFcv_Hiy9DYYs6NtAII2kGOEaOdGTwwBLme9fbiAY3IRQxosfiEE84B6cDNQIdatUs0goVNF4uj7N9x3Zi6aWbu-P3GDw_nS36qGX369uvCZO0f22MGMqMovT4o2IH3a9wD9R2XChs_CM0iwaU7SCjG4iCo7vtK26aAPUCiAdp6jVLC3pbQ6JghrGayWwPj7ttlr2AOzRaznQqRH71jpBWIGtfmrQbwVoo7RrfS6rV5z7AGPQCCaw-dO_iuw6TUt1TYYdNmcMW5Rk1qng0hiWXk2DS7nFUYabKpCL1pfVkiFDyy3u3g="
     MONGO_DB_URL = "mongodb+srv://userbot:userbot@cluster0.x5kew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
