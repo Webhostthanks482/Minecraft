b99ad20a2c8b0a57ccc9e104f4feb7d2e6cdde9f
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-06 10:18:49 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -3,7 +3,7 @@ from typing import Union, Optional
 API_ID: int = 4726773
 API_HASH: str = "ab29fa05db737b202086e4a10c326c41"
 STRING_SESSION: str = "1AZWarzgBu3ecdnighnLB8jtb0rIIZu2UMbrmVLhEiEeGRFcv_Hiy9DYYs6NtAII2kGOEaOdGTwwBLme9fbiAY3IRQxosfiEE84B6cDNQIdatUs0goVNF4uj7N9x3Zi6aWbu-P3GDw_nS36qGX369uvCZO0f22MGMqMovT4o2IH3a9wD9R2XChs_CM0iwaU7SCjG4iCo7vtK26aAPUCiAdp6jVLC3pbQ6JghrGayWwPj7ttlr2AOzRaznQqRH71jpBWIGtfmrQbwVoo7RrfS6rV5z7AGPQCCaw-dO_iuw6TUt1TYYdNmcMW5Rk1qng0hiWXk2DS7nFUYabKpCL1pfVkiFDyy3u3g="
-Sibyl_logs: Union[str, int] =  -1001216545116
-Sibyl_approved_logs: Union[str, int] = -1001216545116
-GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+Sibyl_logs: Union[str, int] =  "1001216545116"
+Sibyl_approved_logs: Union[str, int] = "1001216545116"
+GBAN_MSG_LOGS: Optional[Union[str, int]] = "1001548543332"
 BOT_TOKEN: str = "1729614497:AAFj_7Psr0gV59atqmE8pPWe59xxuCzpurM"
